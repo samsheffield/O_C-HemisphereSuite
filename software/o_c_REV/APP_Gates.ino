e4ac49dcc51dfcbd1f53a38c13cc805bef092f7e
Sam Sheffield <jssheffield@gmail.com>
2021-04-25 18:29:17 -0400

    Gates is now Popcorn
    This app is way too silly to be called Gates. Main functionality is all in there. Needs UI

--- software/o_c_REV/APP_Gates.ino
+++ software/o_c_REV/APP_Gates.ino
@@ -18,6 +18,18 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+// Left encoder:
+// Rotate - set beats for active channel
+// Press - switch channel
+
+// Right encoder:
+// Rotate - set rotation for active channel
+// Press - switch channel
+
+// CV In - override rotation? beats? -0v - beats +0v rotation?
+
+// Something with the buttons...
+
 #include "HSApplication.h"
 #include "HSMIDI.h"
 #include "bjorklund.h"
@@ -25,11 +37,14 @@
 class Gates : public HSApplication, public SystemExclusiveHandler {
 public:
 	void Start() {
+        lchannel = 0;
+        rchannel = 0;
+
         ForEachChannelAll(ch)
         {
             length[ch] = 16;
-            beats[ch] = 4 + (ch * 4);
-            pattern[ch] = EuclideanPattern(length[ch] - 1, beats[ch], 0);;
+            beats[ch] = 4 + (ch * 2); // shortened
+            pattern[ch] = EuclideanPattern(length[ch] - 1, beats[ch], 0);
         }
         step = 0;
 	}
@@ -42,9 +57,9 @@ public:
         if (Clock(0)) {
             ForEachChannelAll(ch)
             {
-                int rotation = Proportion(DetentedIn(ch), HSAPPLICATION_5V, length[ch]);
+                //int rotation = Proportion(DetentedIn(ch), HSAPPLICATION_5V, length[ch]);
 
-                pattern[ch] = EuclideanPattern(length[ch] - 1, beats[ch], rotation);
+                pattern[ch] = EuclideanPattern(length[ch] - 1, beats[ch], rotation[ch]);
                 int sb = step % length[ch];
                 if ((pattern[ch] >> sb) & 0x01) {
                     ClockOut(ch);
@@ -56,8 +71,8 @@ public:
     }
 
     void View() {
-        gfxHeader("Gates");
-        //DrawInterface();
+        gfxHeader("Popcorn");
+        DrawInterface();
     }
 
     void OnSendSysEx() {
@@ -70,6 +85,7 @@ public:
     // Control handlers
     /////////////////////////////////////////////////////////////////
     void OnLeftButtonPress() {
+        lactive[lchannel] = !lactive[lchannel];
     }
 
     void OnLeftButtonLongPress() {
@@ -88,23 +104,93 @@ public:
     }
 
     void OnLeftEncoderMove(int direction) {
+        // If the channel isn't active it can be switched
+        // Once it's active, set the number of beats for it
+        if(!lactive[lchannel]){
+            lchannel += direction;
+            if(lchannel < 0){
+                lchannel = 3;
+            }
+            else if(lchannel > 3){
+                lchannel = 0;
+            }
+        }
+        else{
+            beats[lchannel] += direction;
+            if(beats[lchannel] < 0){
+                beats[lchannel] = 0;
+            }
+            else if(beats[lchannel] > length[lchannel]-1){
+                beats[lchannel] = length[lchannel];
+            }
+        }        
     }
 
     void OnRightEncoderMove(int direction) {
+        // If the channel is active, set the rotation for it
+        if(lactive[lchannel]){
+            rotation[lchannel] += direction;
+            if(rotation[lchannel] < 0){
+                rotation[lchannel] = 0;
+            }
+            else if(rotation[lchannel] > length[lchannel]-1){
+                rotation[lchannel] = length[lchannel];
+            }
+        }  
     }
 
 private:
     int8_t cursor;
+
+    int8_t lchannel;
+    int8_t rchannel;
+
     int step;
     uint32_t pattern[4];
     
     // Settings
     int length[4];
     int beats[4];
-    /* Example private screen-drawing method
+    
+    bool lactive[4];
+    int rotation[4];
+
     void DrawInterface() {
+        gfxPrint(1, TopAlign(2), 0);
+        gfxPrint(1, TopAlign(12), beats[0]);
+        gfxPrint(1, TopAlign(22), rotation[0]);
+
+        gfxPrint(31, TopAlign(2), 1);
+        gfxPrint(31, TopAlign(12), beats[1]);
+        gfxPrint(31, TopAlign(22), rotation[1]);
+
+        gfxPrint(61, TopAlign(2), 2);
+        gfxPrint(61, TopAlign(12), beats[2]);
+        gfxPrint(61, TopAlign(22), rotation[2]);
+
+        gfxPrint(91, TopAlign(2), 3);
+        gfxPrint(91, TopAlign(12), beats[3]);
+        gfxPrint(91, TopAlign(22), rotation[3]);
+
+        switch (lchannel)
+        {
+        case 0:
+            gfxCursor(1,TopAlign(11),12);
+            break;
+        case 1:
+            gfxCursor(31,TopAlign(11),12);
+            break;
+        case 2:
+            gfxCursor(61,TopAlign(11),12);
+            break;
+        case 3:
+            gfxCursor(91,TopAlign(11),12);
+            break;
+        
+        default:
+            break;
+        }
     }
-    */
 };
 
 Gates Gates_instance;
